7f6b3b3f76d2942f24e938549841f429c9301aca
mkwtys <mkwtys@gmail.com>
2018-08-20 15:32:16 +0900

    enable selector-pseudo-element-colon-notation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['stylelint-config-recommended', 'stylelint-config-prettier'],
   rules: {
     'color-hex-length': 'short',
-    'length-zero-no-unit': true
+    'length-zero-no-unit': true,
+    'selector-pseudo-element-colon-notation': 'double'
   }
 };
