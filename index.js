f0ffa75a89d4e9735ef92e1298b672604c7b88ac
mkwtys <mkwtys@gmail.com>
2018-08-20 16:10:18 +0900

    enable at-rule-no-unknown

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 module.exports = {
   extends: ['stylelint-config-recommended', 'stylelint-config-prettier'],
   rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'extend',
+          'at-root',
+          'debug',
+          'warn',
+          'error',
+          'if',
+          'else',
+          'for',
+          'each',
+          'while',
+          'mixin',
+          'include',
+          'content',
+          'return',
+          'function'
+        ]
+      }
+    ],
     'color-hex-length': 'short',
     'length-zero-no-unit': true,
     'selector-pseudo-element-colon-notation': 'double'
