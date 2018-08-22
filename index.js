466eb7ca0bb9fa741c92ba5dce14a881981fd1fc
mkwtys <mkwtys@gmail.com>
2018-08-22 19:12:15 +0900

    enable rules
    - comment-empty-line-before
    - declaration-empty-line-before
    - font-family-name-quotes
    - no-missing-end-of-source-newline
    - no-unknown-animations
    - number-max-precision
    - selector-attribute-operator-space-after
    - selector-attribute-operator-space-before
    - selector-type-case
    - shorthand-property-no-redundant-values
    - value-keyword-case

--- index.js
+++ index.js
@@ -26,12 +26,29 @@ module.exports = {
     ],
     'at-rule-no-vendor-prefix': true,
     'color-hex-length': 'short',
+    'comment-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested'],
+        ignore: ['stylelint-commands']
+      }
+    ],
     'comment-whitespace-inside': 'always',
+    'declaration-empty-line-before': 'never',
+    'font-family-name-quotes': 'always-where-recommended',
     'function-url-quotes': 'always',
     'function-whitespace-after': 'always',
     'length-zero-no-unit': true,
     'media-feature-range-operator-space-after': 'always',
     'media-feature-range-operator-space-before': 'always',
-    'selector-pseudo-element-colon-notation': 'double'
+    'no-missing-end-of-source-newline': true,
+    'no-unknown-animations': true,
+    'number-max-precision': 10,
+    'selector-attribute-operator-space-after': 'never',
+    'selector-attribute-operator-space-before': 'never',
+    'selector-pseudo-element-colon-notation': 'double',
+    'selector-type-case': 'lower',
+    'shorthand-property-no-redundant-values': true,
+    'value-keyword-case': 'lower'
   }
 };
